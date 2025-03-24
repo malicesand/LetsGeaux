d51440d7ea3ccded3b375a2474155e27baac5731
stephen <jimisax@gmail.com>
2025-03-24 17:26:06 -0500

    ADD: modularity to Prisma Db file

--- server/database/index.ts
+++ server/database/index.ts
@@ -3,8 +3,17 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
+  // await prisma.user.create ({
+  //   data: {
+  //     username: 'Bob', 
+  //     email: 'kid2luv@yahoo.com', 
+  //     isVerified: true,
+  //     phoneNum: '3413791',
+  //     isNotified: false,
+  //   },
+  // }),
   const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
+  console.dir(allUsers)
 }
 
 main()
