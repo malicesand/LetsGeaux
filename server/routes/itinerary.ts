61fdec234fcb517aa88bbbdd813ca4e8ab9a83e2
wlegard <whitleylegard.com>
2025-03-25 18:34:50 -0500

    figuring out routes

--- server/routes/itinerary.ts
+++ server/routes/itinerary.ts
@@ -1,16 +1,21 @@
 import express from 'express';
 import {Request, Response} from 'express'
-//TODO: import { Itenarary } from ''
+//TODO: import { Prisma} from '@prisma'
+ //TODO: import { Itinerary } from '../database/index'
 
 const route = express.Router();
 
-route.get('/', async (req: any ,res: any )=>{
-
+route.get('/', async (req: Request, res: Response )=>{
+//const {} = req.params
 try{
-
+  //const itineraries = await Itinerary.find({})
+res.status(200).json()
 }catch(error){
 res.status(500).json({error: 'Error fetching itinerary'})
 }
 
 })
 
+route.post('/', async (req: Request, res: Response) =>{
+  
+}) 
