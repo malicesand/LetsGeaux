7dbb6ab985dbeb921072466cfdc99be80f62012f
wlegard <whitleylegard.com>
2025-03-25 16:35:55 -0500

    modifying primsa

--- prisma/migrations/20250324212529_init/migration.sql
+++ prisma/migrations/20250324212529_init/migration.sql
@@ -5,16 +5,16 @@
 
 */
 -- DropTable
-DROP TABLE `Test`;
+-- DROP TABLE `Test`;
 
 -- CreateTable
-CREATE TABLE `user` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(50) NOT NULL,
-    `email` VARCHAR(50) NOT NULL,
-    `isVerified` BOOLEAN NOT NULL,
-    `phoneNum` VARCHAR(10) NOT NULL,
-    `isNotified` BOOLEAN NOT NULL,
+-- CREATE TABLE `user` (
+--     `id` INTEGER NOT NULL AUTO_INCREMENT,
+--     `username` VARCHAR(50) NOT NULL,
+--     `email` VARCHAR(50) NOT NULL,
+--     `isVerified` BOOLEAN NOT NULL,
+--     `phoneNum` VARCHAR(10) NOT NULL,
+--     `isNotified` BOOLEAN NOT NULL,
 
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+--     PRIMARY KEY (`id`)
+-- ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
