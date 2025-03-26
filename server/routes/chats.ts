25257e14536576440fe9a86813dcd0266cdf97ad
malicesand <malicesand@gmail.com>
2025-03-26 11:32:58 -0500

    Add cors and change start script

---
+++ server/routes/chats.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response} from 'express';
+import cors from 'cors';
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+import { PromptTemplate } from "langchain/prompts";
+import { StringOutputParser } from "langchain/schema/output_parser";
+
+
+import axios from 'axios'
+//prisma goes here
+
+const chatsRoute = express.Router()
+const apiKey = process.env.GOOGLE_API_KEY;
+
+ const llm = new GoogleGenerativeAI({
+   model: string: "gemini-1.5-flash",
+   temperature: 0,
+   maxRetries: 2,
+   // apiKey: "...",
+   // other params...
+ });
