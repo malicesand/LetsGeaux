8f55db974db605195b95a639e62932d512ee6845
taytay836 <javanteshanks@yahoo.com.com>
2025-03-27 18:55:00 -0500

    added Budget Buddy Folder seperated into sub components and added layout of subs

---
+++ src/client/components/BudgetBuddy/BudgetBuddy.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Container, Typography, Paper } from '@mui/material';
+import BudgetOverview from './BudgetOverview';
+import BudgetForm from './BudgetForm';
+import BudgetCategories from './BudgetCategories';
+//layout for budget page
+//keep track of organizing sub components
+const BudgetBuddy: React.FC = () => {
+  return (
+    <Container sx={{ py: 5 }}>
+      <Typography variant="h3" gutterBottom align="center">
+        budgetBuddy
+      </Typography>
+
+      {/* Budget Summary */}
+      <Paper elevation={3} sx={{ p: 3, mb: 4 }}>
+        <BudgetOverview />
+      </Paper>
+
+      {/* Budget creation form */}
+      <Paper elevation={3} sx={{ p: 3, mb: 4 }}>
+        <BudgetForm />
+      </Paper>
+
+      {/* Budget categories */}
+      <Paper elevation={3} sx={{ p: 3 }}>
+        <BudgetCategories />
+      </Paper>
+    </Container>
+  );
+};
+
+export default BudgetBuddy;
