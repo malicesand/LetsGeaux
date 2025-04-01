fe6581738e465e8b726dd5039b1e867fb540d0af
malicesand <malicesand@gmail.com>
2025-03-31 22:15:08 -0500

    Fix Oauth and create get request for user information on App.tsx

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,91 +1,94 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
   Routes,
   Route,
-  BrowserRouter as Router, 
-  useParams, 
-  useNavigate, 
-  Navigate,
-  useLocation,
-  Link  } from 'react-router-dom';
+  Navigate, } from 'react-router-dom';
 import axios from 'axios';
 
 
 // feature routes
 import Home from './Home.tsx';
 import Login from './Login.tsx';
-import NavDrawer from './NavDrawer.tsx'; // 
 import Maps from './Maps.tsx'
 import Suggestions from './Suggestions.tsx'
 import ChatBot from './ChatBot.tsx'
-//! add other componenent imports BELOW this line
 import Itinerary from './Itineraray.tsx';
 import Calendar from './Calendar.tsx';
 import BudgetBuddy from './BudgetBuddy/BudgetBuddy.tsx';
 
-
-// interface ISetAuth {
-  //   setAuth: 
-  // }
-  
-  // const Login = ({ setAuth }: ISetAuth) => {
-    
-  // }
-  
-  // States
   
   
-  const App: React.FC = () => {
+const App: React.FC = () => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   // Check Auth
   useEffect(() => {
-    const checkAuth = async (params: any) => { //? type 
+    const checkAuth = async () => { //? type 
       try {
-        const response = await axios.get('/api/check/auth');
+        const response = await axios.get('/api/check-auth');
         setIsAuthenticated(response.data.isAuthenticated);
+
+        if (response.data.isAuthenticated) {
+          console.log(response.data.user);
+        }
       }
       catch (error) {
         setIsAuthenticated(false);
         console.error('Server: Err checking auth status', error);
       }
     };
-  
-    // checkAuth(); 
-  }, []); // ? need empty array?
+    checkAuth(); 
+  }, []); 
   
   // Protected Route
-  const ProtectedRoute = (children: any) => {
+  const ProtectedRoute: React.FC<{children: React.ReactNode}> = ({children}) => {
     if (!isAuthenticated) {
-      return <Navigate to="/" replace />;
+      return <Navigate to="/login" replace />;
     }
-    return children
+    return <>{children}</>
   };
   
 
   return (
-    // <div>
-      //  <Router>
+    <Routes>
+
+      <Route path="/login" element={!isAuthenticated ? <Login /> :  <Navigate to="/" replace />} /> 
       
-      <Routes>
-        {/* <NavDrawer setIsAuthenticated={setIsAuthenticated} /> */}
-     
-       <Route path="login" element={!isAuthenticated ? <Login /> :  <Navigate to="/" />} /> 
-
-        <Route path="/" element={ <Home/> }/>
-        {/* <Route path="/" element={<NavDrawer/>}/> */}
-        <Route path="/maps" element={<Maps/>}/>
-        <Route path="/suggestions" element={<Suggestions/>}/>
-        <Route path="/chatbot" element={<ChatBot/>}/>
-        {/* add other paths BELOW this line */}
-        
-        <Route path="/itinerary" element={<Itinerary/>}/>
-        <Route path="/calendar" element={<Calendar />} />
-
-        <Route path="/budgetbuddy" element={<BudgetBuddy />} />
-
-      </Routes>
-// </Router>
-    /* </div> */
+      <Route path="/" element={ 
+        <ProtectedRoute>
+          <Home /> 
+        </ProtectedRoute>
+      }/>
+      <Route path="/maps" element={
+        <ProtectedRoute>
+          <Maps/>
+        </ProtectedRoute>
+      }/>
+      <Route path="/suggestions" element={
+        <ProtectedRoute>
+          <Suggestions/>
+        </ProtectedRoute>
+      }/>
+      <Route path="/chatbot" element={
+        <ProtectedRoute>
+          <ChatBot/>
+        </ProtectedRoute>
+      }/>
+      <Route path="/itinerary" element={
+        <ProtectedRoute>
+          <Itinerary/>
+        </ProtectedRoute>
+      }/>
+      <Route path="/calendar" element={
+        <ProtectedRoute>
+          <Calendar />
+        </ProtectedRoute> 
+      }/>
+      <Route path="/budgetbuddy" element={
+        <ProtectedRoute> 
+          <BudgetBuddy />
+        </ProtectedRoute>
+      }/>
+    </Routes>
   )
 };
 
