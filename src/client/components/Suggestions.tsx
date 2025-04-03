a41692d2c2470d56dca9166c6c6df185061ac77c
stephen <jimisax@gmail.com>
2025-04-03 08:28:30 -0500

    REFACTOR: Suggestion.tsx and SuggestionForm.tsx to align with suggestion schema model

--- src/client/components/Suggestions.tsx
+++ src/client/components/Suggestions.tsx
@@ -1,55 +1,50 @@
-// import React from 'react';
-// import axios from 'axios';
-// import ActivityForm from './ActivityForm';
-// import { Container, Card, Typography, Dialog, Grid, Avatar, } from '@mui/material'
-// import Suggestion from './Suggestion';
-
-// // const Grid = Grid2;
-// const Suggestions = () => {
-//   return (
-//     <Container>
-//       <Suggestion />
-//     </Container>
-//   )
-// }
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import { Container, Card, Button, Input, InputLabel, Typography, Accordion, Grid,  } from '@mui/material';
-import Activity from './Activity';
-import ActivityForm from './ActivityForm';
+import SuggestionForm from './SuggestionForm';
+import { Container, Card, Typography, Dialog, Grid, Avatar, Input, InputLabel, Button, SpeedDial } from '@mui/material'
+import Suggestion from './Suggestion';
 
+// const Grid = Grid2;
+const Suggestions = () => {
 
-const Activities = () => {
-  const [activitySet, setActivitySet] = useState([]);
-  const [editMode, setEditMode] = useState(false);
+  const [editableSuggestion, setEditableSuggestion] = useState({});
+  const [suggestionSet, setSuggestionSet] = useState([]);
+  const [suggestionEditMode, setSuggestionEditMode] = useState(false);
 
-  const getAllActivities = () => {
+  const getAllSuggestions = () => {
     axios.get('/api/activity').then(({data}) => {
       console.log(data);
-      setActivitySet(data);
+      setSuggestionSet(data);
     }).catch((err) => console.error('there was an issue', err));
   }
 
   useEffect(() => {
-    getAllActivities();
+    getAllSuggestions();
   }, []);
+
   return (
     <Container>
-      <h2>Current Activities</h2>
-      {activitySet.map((act) => (
-        <Card key={act.name}>
-      <Activity
-      act={act}
-      getAllActivities={getAllActivities}
-      setEditMode={setEditMode}
-      setActivitySet={setActivitySet}
-      />
-      </Card>
+      <h1>Suggested Excursions</h1>
+      {suggestionSet.map((sugg) => (
+        <Card key={sugg.id}>
+          <Suggestion 
+          sugg={sugg}
+          getAllSuggestions={getAllSuggestions}
+          setSuggestionEditMode={setSuggestionEditMode}
+          setSuggestionSet={setSuggestionSet}
+          setEditableSuggestion={setEditableSuggestion}
+          />
+        </Card>
       ))}
-<ActivityForm activitySet={activitySet} editMode={editMode} setEditMode={setEditMode}/>
+      <SuggestionForm
+suggestionSet={suggestionSet}
+suggestionEditMode={suggestionEditMode}
+setSuggestionEditMode={setSuggestionEditMode}
+getAllSuggestions={getAllSuggestions}
+editableSuggestion={editableSuggestion}
+/>
     </Container>
   )
 }
 
-// export default Activities;
-export default Activities;
+export default Suggestions;
