d0937dcfa9dc8ad39764de8ccaad025c4826d41c
malicesand <malicesand@gmail.com>
2025-04-08 19:28:00 -0500

    Hide AppBar when user is not logged in. Pass down User to Appbar for access to default profile image.

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './client/components/App';
-import MainAppBar from './client/components/AppBar';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <MainAppBar />   
     <App />
   </BrowserRouter>
 );
