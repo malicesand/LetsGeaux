7007a9cb3bf7ba166e980a0220e42e0d62005971
taytay836 <javanteshanks@yahoo.com.com>
2025-04-10 17:21:15 -0500

    wrapped the entire app in notistack and added notistack in app bar

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './client/components/App';
+import { SnackbarProvider } from 'notistack';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+  <SnackbarProvider maxSnack={3}>
     <App />
-  </BrowserRouter>
+  </SnackbarProvider>
+</BrowserRouter>
 );
