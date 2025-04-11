4a3e3226e318c3540763e52aa93008ff92f57338
stephen <jimisax@gmail.com>
2025-04-11 16:57:41 -0500

    ADD: CommunityPage directory and relevant files

--- src/client/components/Community/CommunityPage.tsx
+++ src/client/components/Community/CommunityPage.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 // import axios from 'axios';
-import Suggestion from './Suggestion.tsx';
+import Suggestion from '../Suggestion.tsx';
 import { Container, Typography, List, Card, Paper, Grid} from '@mui/material';
 
-const Community = () => {
+const CommunityPage = () => {
   return (
     <Container>
       <Typography variant='h1'>Welcome to da pawty!</Typography>
@@ -13,4 +13,4 @@ const Community = () => {
   )
 }
 
-export default Community;
+export default CommunityPage;
