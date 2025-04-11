757045b28b5016bde47a6f0db7ab5ebb0caa502c
stephen <jimisax@gmail.com>
2025-04-11 16:35:05 -0500

    ADD: Community file

--- src/client/components/Community.tsx
+++ src/client/components/Community.tsx
@@ -1,3 +1,16 @@
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import Suggestion from './Suggestion.tsx';
+import { Container, Typography, List, Card, Paper, Grid} from '@mui/material';
+
+const Community = () => {
+  return (
+    <Container>
+      <Typography variant='h1'>Welcome to da pawty!</Typography>
+      <Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+export default Community;
