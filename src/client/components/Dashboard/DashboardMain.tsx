531b17835b07ee9c7948c9b4b66ec611e5872deb
malicesand <malicesand@gmail.com>
2025-04-11 19:30:12 -0500

    Create message board feature for the Travel Party dashboard

--- src/client/components/Dashboard/DashboardMain.tsx
+++ src/client/components/Dashboard/DashboardMain.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import Box from '@mui/material/Box';
+import MessageBoard from './MessageBoard';
+import CreateGroup from './CreateGroup';
+import ReplyForm from './ReplyForm'
+import BudgetPieChart from '../BudgetBuddy/BudgetPieChart';
 
+import MessageList from './MessageList';
 
 import AddMember from './AddMember';
-import CreateGroup from './CreateGroup';
-import BudgetPieChart from '../BudgetBuddy/BudgetPieChart';
 
 import { user } from '../../../../types/models.ts';
 
@@ -18,8 +21,13 @@ const Dashboard: React.FC<DashboardProps>= ({ user }) => {
   return (
     <Box>
       <CreateGroup user={user}/>
+      <Box>
+        <MessageBoard user={user}/>
+        
+        
+      </Box>
       <Box position= 'absolute' bottom = '10%'>
-        <BudgetPieChart />
+        {/* <BudgetPieChart /> */}
       </Box>
     </Box>
   )
