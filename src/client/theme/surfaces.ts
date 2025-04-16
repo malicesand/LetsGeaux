d8e4a0aabc2c5867b03bf67170a9d493f8a213f1
taytay836 <javanteshanks@yahoo.com.com>
2025-04-16 14:29:59 -0500

    added theme provider and wrapped app in it

--- src/client/theme/surfaces.ts
+++ src/client/theme/surfaces.ts
@@ -1,4 +1,46 @@
+import { Components } from '@mui/material/styles';
+
+export const components: Components = {
+  MuiContainer: {
+    defaultProps: { maxWidth: 'lg' },
+    styleOverrides: {
+      root: { paddingLeft:  16, paddingRight: 16 },
+    },
+  },
+  MuiTextField: {
+    defaultProps: { variant: 'outlined', size: 'medium', fullWidth: true, margin: 'normal' },
+    styleOverrides: {
+      root: { marginBottom: 16 },
+    },
+  },
+  MuiCard: {
+    styleOverrides: {
+      root: ({ theme }) => ({
+        padding:      theme.spacing(2),
+        borderRadius: theme.shape.borderRadius,
+        boxShadow:    theme.shadows[4],
+      }),
+    },
+  },
+  MuiDialog: {
+    styleOverrides: {
+      paper: ({ theme }) => ({
+        padding:      theme.spacing(3),
+        borderRadius: theme.shape.borderRadius,
+      }),
+    },
+  },
+  MuiButton: {
+    defaultProps: { disableElevation: true },
+    styleOverrides: {
+      root: ({ theme }) => ({
+        borderRadius: theme.shape.borderRadius,
+        padding:      theme.spacing(1, 2),
+      }),
+    },
+  },
+};
+
 //TODO
   //* Rounded Corners
-  //? Paper 
-  
+  //? Paper
