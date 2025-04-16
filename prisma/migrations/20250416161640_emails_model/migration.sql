077c1b5c3d924f7676ab2003efe7e3c9ebcf8009
malicesand <malicesand@gmail.com>
2025-04-16 11:18:58 -0500

    Migrate emails-model

---
+++ prisma/migrations/20250416161640_emails_model/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `emails` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `address` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `emails` ADD CONSTRAINT `emails_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
