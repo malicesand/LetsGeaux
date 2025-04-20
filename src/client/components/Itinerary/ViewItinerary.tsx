c8e8e33a3c2b78cbc5e4b488f44570ba5182c83a
wlegard <whitleylegard.com>
2025-04-19 19:05:22 -0500

    added more on viewItinerary

--- src/client/components/Itinerary/ViewItinerary.tsx
+++ src/client/components/Itinerary/ViewItinerary.tsx
@@ -2,8 +2,9 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {useParams} from 'react-router-dom';
-import {Container, Typography} from '@mui/material'
-const viewItinerary = () => {
+import {Alert, Box, Container, Typography} from '@mui/material'
+
+const ViewItinerary = () => {
 const [itinerary, setItinerary] = useState<any>(null);
   const [error, setError] = useState<string>('');
 const {viewCode} = useParams();
@@ -31,22 +32,70 @@ if(viewCode){
 
 },[viewCode])
 
-//TODO: if no itinerary
+if (error) {
+  
+  return (
+    <Container>
+      <Alert severity="error">{error}</Alert>
+    </Container>
+  );
+}
+
+if (!itinerary) {
+  return (
+    <Container>
+      <Typography variant="h6">Loading itinerary...</Typography>
+    </Container>
+  );
+}
+
+
+
+
 
 return (
   <Container>
-<Typography variant='h6'></Typography>
-<Typography variant='body1'></Typography>
-<Typography></Typography>
+    <Typography variant="h4" gutterBottom>
+      {itinerary.name}
+    </Typography>
 
-    {/*  TODO:
-    itinerary.name
-    itienarary.notes
-    itinerary.activities
-     */}
+    {itinerary.party?.name && (
+      <Typography variant="subtitle1" color="textSecondary" gutterBottom>
+        Party: {itinerary.party.name}
+      </Typography>
+    )}
 
+    <Typography variant="body1" gutterBottom>
+      Notes: {itinerary.notes}
+    </Typography>
 
+    <Typography variant="body2" gutterBottom>
+      Start: {new Date(itinerary.begin).toLocaleString()}
+    </Typography>
+    <Typography variant="body2" gutterBottom>
+      End: {new Date(itinerary.end).toLocaleString()}
+    </Typography>
+
+    <Box mt={4}>
+      <Typography variant="h6">Activities</Typography>
+      {itinerary.activity && itinerary.activity.length > 0 ? (
+        itinerary.activity.map((activity: any) => (
+          <Box key={activity.id} mb={2} p={2} border="1px solid #ccc" borderRadius={2}>
+            <Typography variant="subtitle1">{activity.name}</Typography>
+            <Typography>{activity.description}</Typography>
+            <Typography>{activity.date} at {activity.time}</Typography>
+            <Typography>{activity.location}</Typography>
+            <Typography>{activity.phone}</Typography>
+            <Typography>{activity.address}</Typography>
+          </Box>
+        ))
+      ) : (
+        <Typography>No activities added yet.</Typography>
+      )}
+    </Box>
   </Container>
-)
-}
-export default viewItinerary;
+);
+};
+
+
+export default ViewItinerary;
