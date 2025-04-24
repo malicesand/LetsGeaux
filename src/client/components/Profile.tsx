7090b4f1dad9ab481e3b4a87c587ffd7d3586b31
mydogditto <dhgoldberg15@gmail.com>
2025-04-24 11:39:17 -0500

    ADD pencil icon & moved to overlap Profileimage

--- src/client/components/Profile.tsx
+++ src/client/components/Profile.tsx
@@ -16,6 +16,7 @@ import {
 } from '@mui/material';
 import ImageUpload from './ImageUpload';
 import { useUser } from './UserContext';
+import { PiPencilBold } from "react-icons/pi";
 
 const ALL_INTERESTS = ['Restaurants', 'Hotels', 'Geos', 'Attractions'];
 
@@ -100,19 +101,47 @@ const Profile: React.FC = () => {
   return (
     <Container maxWidth="sm" sx={{ py: 4 }}>
       <Typography variant="h2" gutterBottom align="center">Profile</Typography>
+
       <Stack spacing={2} alignItems="center">
-        <Avatar
-          src={contextUser.profilePic}
-          alt="Profile Picture"
-          sx={{ width: 150, height: 150, border: '4px solid black' }}
-        />
+        {/* Wrapper box to position the edit icon */}
+        <Box sx={{ position: 'relative', width: 150, height: 150 }}>
+          <Avatar
+            src={contextUser.profilePic}
+            alt="Profile Picture"
+            sx={{
+              width: 150,
+              height: 150,
+              border: '4px solid black',
+              boxShadow: 1,
+              transition: 'box-shadow 0.3s ease-in-out',
+              '&:hover': {
+                boxShadow: 6,
+                bgcolor: '#fffff', // kept your original color
+              },
+            }}
+          />
 
+          <Button
+            variant="contained"
+            onClick={handleUploadWidget}
+            sx={{
+              position: 'absolute',
+              bottom: 0,
+              right: 0,
+              minWidth: 0,
+              width: 40,
+              height: 40,
+              borderRadius: '50%',
+              p: 0,
+              color: 'primary',
+            }}
+          >
+            <PiPencilBold />
+          </Button>
+        </Box>
 
-        <Button variant="contained" onClick={handleUploadWidget}
-          sx={{ mt: 2, color: 'primary' }}>
-
-          Change Profile Picture</Button>
-        <Typography variant="body1" textTransform="uppercase">{contextUser.username}</Typography><Typography variant="body1">{contextUser.email}</Typography>
+        <Typography variant="body1" textTransform="uppercase">{contextUser.username}</Typography>
+        <Typography variant="body1">{contextUser.email}</Typography>
       </Stack>
 
       <Box mt={4}
@@ -122,8 +151,7 @@ const Profile: React.FC = () => {
           padding: 4,
           maxWidth: 300,
           textAlign: 'center',
-
-          mx: 'auto'
+          mx: 'auto',
         }}
       >
         <Typography variant="h5" textTransform="uppercase" fontWeight='700' >Current Interest:</Typography>
@@ -158,7 +186,7 @@ const Profile: React.FC = () => {
 
       </Box>
       <Box mt={4}>
-        {/* <ImageUpload userId={contextUser.id} /> */}
+
       </Box>
     </Container>
   );
